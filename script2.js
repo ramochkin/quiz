e5bc1e7a81419cf25d4e3a2c481aa1eda6f7b024
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-01-27 15:18:41 -0500

    scoreboard organized  numerically

--- script2.js
+++ script2.js
@@ -2,7 +2,13 @@ var scoreBoard = document.getElementById("#scoreBoard")
 
 //Pull data out of local storage
 var scores = JSON.parse(window.localStorage.getItem("highscores"))
+scores = scores.sort(function(score1, score2){
+    //console.log("One Score", score1);
+    //console.log("Score Two ", score2);
+    return score2.score - score1.score;
+});
 console.log(scores);
+// console.log(scores[0]);
 
 //var scores is the data. create a function to sort the data based on index, 
 // inject it into it's own line in the organised list defines as scoreBoard.
@@ -11,14 +17,15 @@ var activeScore = 0
 
 for (let index = 0; index < scores.length; index++) {
     var score = scores[activeScore];
+    console.log(score)
     activeScore++;
     addElement(score);
-    //console.log(score)
+    console.log(score.score)
 }
 
-function addElement() {
+function addElement(score) {
     var newLi = document.createElement("li");
-    var newLiContent = document.createTextNode(score)
+    var newLiContent = document.createTextNode(score.initials + " " + score.score);
     newLi.appendChild(newLiContent);
 
     var currentOl = document.scoreBoard;
@@ -26,4 +33,4 @@ function addElement() {
 
 }
 
-addElement();
+
